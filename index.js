6658082324332af99573c18e68571018f51adc4c
stabldev <114811070+stabldev@users.noreply.github.com>
2025-08-03 09:40:55 +0530

    chore: add jsdocs

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 import fs from "fs";
 import path from "path";
 
+/**
+ * Returns the version of an installed npm package.
+ * @param { string } pkgName The name of the package.
+ * @param { string } [baseDir=process.cwd()] Optional base directory (defaults to current working dir).
+ * @returns { string | null } version or null if not found.
+ */
 export default function pkgversion(pkgName, baseDir = process.cwd()) {
   try {
     const pkgPath = path.resolve(
